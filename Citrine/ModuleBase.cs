ce56a20ab4d9eb8acf85fb0cc550af8a2c23180d
Xeltica <citringo@gmail.com>
2019-01-03 13:00:22 +0900

    initial

---
+++ Citrine/ModuleBase.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Disboard.Misskey;
+using Disboard.Misskey.Models;
+
+namespace Citrine
+{
+	/// <summary>
+	/// Citrine のモジュールベース。
+	/// </summary>
+	public abstract class ModuleBase
+	{
+		public virtual int Priority => 0;
+
+		public virtual async Task<bool> ActivateAsync(Note n, MisskeyClient mi, Citrine core) => false;
+
+		public virtual async Task<bool> OnTimelineAsync(Note n, MisskeyClient mi, Citrine core) => false;
+	}
+
+	public class GreetingModule : ModuleBase
+	{
+		(string pattern, string reply, string replyLove, string replyHate)[] greetingTable =
+		{
+			("お(はよ|早)う?", "おはようございます.", "おはよ", null),
+			("こんにちは", "こんにちは.", "こんにちは〜", null),
+			("こんばんは",  "こんばんは.", "こんばんは〜", null),
+			("おやす", "おやすみなさい.", "おやす〜", null),
+			("(ねむ|眠)い", "寝ましょう?", "一緒にねよ?", "...寝れば?"),
+			("つらい", "なでなで", "だいじょうぶ? こ, こっちおいで...", "かわいそ."),
+			("(いた|痛)い", "大丈夫ですか?", "だいじょうぶ? さすさす...", "かわいそ."),
+			("ありがと", "どういたしまして.", "いえいえ〜", "はい."),
+			("[す好]き", "あ, ありがとう.", "僕もすきだよ〜", "そう."),
+			("なでなで", "わっ, びっくりした...", "えへへ.", "は?"),
+			("ぎゅ", "え, え, ちょっ", "ぎゅ〜.", "もう, やめて"),
+			("お(なか|腹)[す空]いた", "ご飯たべましょ〜.", "なんかつくりましょうか!", "...食べてくれば."),
+			("(可愛|かわい)い", "て、照れます...", "嬉しい❤", "...褒めても何も出ないけど."),
+			("ping", "PONG!", null, null),
+		};
+
+		public override async Task<bool> ActivateAsync(Note n, MisskeyClient mi, Citrine core)
+		{
+			if (n.Text == null)
+				return false;
+
+			var rec = greetingTable.FirstOrDefault(record => Regex.IsMatch(n.Text.Replace("にゃ", "な"), record.pattern));
+
+			if (rec.pattern == null)
+				return false;
+
+			await Task.Delay(1000);
+
+			// hack 好感度システムを実装したら連携して分岐する
+			await mi.Notes.CreateAsync(
+				core.IsMaster(n.User) ? rec.replyLove ?? rec.reply : rec.reply,
+				replyId: n.Id
+			);
+
+			return true;
+		}
+	}
+
+	public class VoteModule : ModuleBase
+	{
+		Random rnd = new Random();
+		public override async Task<bool> OnTimelineAsync(Note n, MisskeyClient mi, Citrine core)
+		{
+			if (n.Poll == null)
+				return false;
+
+			await Task.Delay(1000);
+
+			// ランダムで投票する
+			await mi.Notes.Polls.VoteAsync(n.Id, rnd.Next(n.Poll.Choices.Count()));
+
+			// 多分競合しないから常にfalse
+			return false;
+		}
+	}
+}
---
+++ Citrine/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Citrine
+{
+	class Program
+	{
+		const string ConfigPath = "./config";
+		static async Task Main(string[] args)
+		{
+			string domain = null, token = null;
+			try
+			{
+				if (File.Exists(ConfigPath))
+				{
+					var cp = File.ReadAllLines(ConfigPath);
+					if (cp.Length < 2)
+						throw new Exception("設定ファイルが不正です。");
+					domain = cp[0];
+					token = cp[1];
+				}
+				else
+				{
+					throw new Exception(""); 
+				}
+			}
+			catch (Exception ex)
+			{
+				// ファイルがない場合やエラーのときはここでトークンを取り直す
+				if (!string.IsNullOrEmpty(ex.Message))
+					Console.WriteLine("エラーが発生しました: " + ex.Message);
+
+			}
+			await Citrine.InitializeAsync(
+				new GreetingModule(),
+				new VoteModule() 
+			);
+
+			while (true)
+				await Task.Delay(1000);
+		}
+	}
+}
