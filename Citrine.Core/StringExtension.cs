13e5cc3daf8e23741081217a761bc11047269599
Xeltica <citringo@gmail.com>
2019-01-24 17:06:41 +0900

    なんか増やした

---
+++ Citrine.Core/StringExtension.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Citrine.Core
+{
+	public static class StringExtension
+	{
+		public static bool IsMatch(this string input, string pattern) => Regex.IsMatch(input, pattern);
+	}
+}
