6e9873a2af345933c8d234da3a7690b94dea9712
Xeltica <citringo@gmail.com>
2019-01-27 12:19:13 +0900

    #わーーーーーーーーーーーーーーー

--- Citrine.Core/LinqExtension.cs
+++ Citrine.Core/LinqExtension.cs
@@ -11,5 +11,22 @@ namespace Citrine.Core
 		public static T Random<T>(this IList<T> list, Random r = null) => list.Count == 0 ? default : list[(r ?? LinqExtension.r).Next(list.Count)];
 
 		public static T Random<T>(this IEnumerable<T> list, Random r = null) => !list.Any() ? default : list.Skip((r ?? LinqExtension.r).Next(list.Count() - 1)).First();
+
+		public static IEnumerable<char> Wa()
+		{
+			yield return '#';
+			yield return 'わ';
+
+			while (true) yield return 'ー';
+		}
+
+		public static IEnumerable<char> Wa(int length)
+		{
+			yield return '#';
+			yield return 'わ';
+
+			for (int i = 0; i < length; i++) yield return 'ー';
+		}
+
 	}
 }
