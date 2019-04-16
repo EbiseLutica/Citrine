59b5b7349ac5cfd2ea0b938aadce244e3e139ce5
Xeltica <citringo@gmail.com>
2019-04-16 13:49:13 +0900

    Prepare Mastodon

---
+++ Citrine.Mastodon/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Disboard.Mastodon;
+using Disboard.Mastodon.Enums;
+
+namespace Citrine.Mastodon
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var cli = new MastodonClient(Domain);
+			var scope = AccessScope.Read | AccessScope.Write | AccessScope.Follow;
+			
+		}
+
+		private static readonly string Domain = "botdon.net";
+	}
+}
