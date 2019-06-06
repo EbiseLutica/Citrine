53f1fde8677228afb55384fb05e7cd3a1f373133
Xeltica <xeltica@gmail.com>
2019-06-07 08:33:46 +0900

    isadmin command

---
+++ Citrine.Core/Commands/IsAdminCommand.cs
@@ -0,0 +1,20 @@
+#pragma warning disable CS1998 // 非同期メソッドは、'await' 演算子がないため、同期的に実行されます
+using System.Threading.Tasks;
+using Citrine.Core.Api;
+
+namespace Citrine.Core
+{
+	public class IsAdminCommand : CommandBase
+	{
+		public override string Name => "isadmin";
+
+		public override string Usage => "/isadmin";
+
+		public override string Description => "管理者であるかどうか取得します";
+
+		public override async Task<string> OnActivatedAsync(ICommandSender sender, Server core, IShell shell, string[] args, string body)
+		{
+			return sender.IsAdmin ? "yes" : "no";
+		}
+	}
+}
