4d22f454e19ffb9ff1b400c3ae8dc6b84ba783b4
Xeltica <xeltica@gmail.com>
2019-06-11 00:16:37 +0900

    ゆうさく

--- Citrine.Discord/Program.cs
+++ Citrine.Discord/Program.cs
@@ -4,6 +4,8 @@ using Citrine.Core;
 
 namespace Citrine.Discord
 {
+	using System.Linq;
+	using Citrine.Core.Api;
 	using static Console;
 	class Program
     {
@@ -15,9 +17,56 @@ namespace Citrine.Discord
 			WriteLine();
 			WriteLine("起動中...");
 			var sh = await Shell.InitializeAsync();
+			sh.Core.AddCommand(new YuusakuCommand());
 			WriteLine("起動しました！");
 
 			await Task.Delay(-1);
         }
     }
+
+	public class YuusakuCommand : CommandBase
+	{
+		public override string Name => "yuusaku";
+
+		public override string Usage => "/yuusaku";
+
+		public override string Description => "コマンドには気をつけよう！";
+
+		public override async Task<string> OnActivatedAsync(ICommandSender sender, Server core, IShell shell, string[] args, string body)
+		{
+			var t = animations.First();
+			var s = shell as Shell;
+			var c = s.CurrentChannel;
+
+			var p = await c.SendMessageAsync(t);
+
+			await animations.Skip(1)
+							.ForEach(async a =>
+							{
+								await p.ModifyAsync(msg => msg.Content = a);
+								await Task.Delay(250);
+							});
+
+			return "";
+		}
+
+		private string[] animations = {
+			".　🙄　　　　　　🐝",
+			".　🙄　　　　　🐝",
+			".　🙄　　　　🐝",
+			".　🙄　　　🐝",
+			".　🙄　　🐝",
+			".　🙄　🐝",
+			".　😒🐝",
+			".　🐝",
+			".🐝😱",
+			".　😨",
+			".　😱",
+			".　😨",
+			".　💀",
+			".　💀",
+			".　💀",
+			"スズメバチには気をつけよう！\n🙄 🙄 🙄",
+		 };
+	}
 }
