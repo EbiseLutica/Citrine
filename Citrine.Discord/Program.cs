9de8eae7046d2cded3fb77ba2349852648f66e5e
Xeltica <xeltica@gmail.com>
2019-06-11 22:13:31 +0900

    YuusakuCommand のファイル分け

--- Citrine.Discord/Program.cs
+++ Citrine.Discord/Program.cs
@@ -4,7 +4,6 @@ using Citrine.Core;
 
 namespace Citrine.Discord
 {
-	using System.Linq;
 	using Citrine.Core.Api;
 	using static Console;
 	class Program
@@ -23,50 +22,4 @@ namespace Citrine.Discord
 			await Task.Delay(-1);
         }
     }
-
-	public class YuusakuCommand : CommandBase
-	{
-		public override string Name => "yuusaku";
-
-		public override string Usage => "/yuusaku";
-
-		public override string Description => "コマンドには気をつけよう！";
-
-		public override async Task<string> OnActivatedAsync(ICommandSender sender, Server core, IShell shell, string[] args, string body)
-		{
-			var t = animations.First();
-			var s = shell as Shell;
-			var c = s.CurrentChannel;
-
-			var p = await c.SendMessageAsync(t);
-
-			await animations.Skip(1)
-							.ForEach(async a =>
-							{
-								await p.ModifyAsync(msg => msg.Content = a);
-								await Task.Delay(250);
-							});
-
-			return "";
-		}
-
-		private string[] animations = {
-			".　🙄　　　　　　🐝",
-			".　🙄　　　　　🐝",
-			".　🙄　　　　🐝",
-			".　🙄　　　🐝",
-			".　🙄　　🐝",
-			".　🙄　🐝",
-			".　😒🐝",
-			".　🐝",
-			".🐝😱",
-			".　😨",
-			".　😱",
-			".　😨",
-			".　💀",
-			".　💀",
-			".　💀",
-			"スズメバチには気をつけよう！\n🙄 🙄 🙄",
-		 };
-	}
 }
