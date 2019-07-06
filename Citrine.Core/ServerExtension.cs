c0bd7af843c0119f33a738ce464ad144e130212f
Xeltica <xeltica@gmail.com>
2019-07-06 13:17:49 +0900

    好感度系拡張メソッド

---
+++ Citrine.Core/ServerExtension.cs
@@ -0,0 +1,35 @@
+using System;
+using Citrine.Core.Api;
+
+namespace Citrine.Core
+{
+    public static class ServerExtension
+	{
+		public static void LikeWithLimited(this Server core, IUser user, int incrementation = 1)
+		{
+			var span = DateTimeOffset.Now - core.Storage[user].Get(StorageKey.LastPlayingDate, DateTimeOffset.Now);
+			if (span < new TimeSpan(0, 30, 0))
+				return;
+			core.Like(user.Id, incrementation);
+			core.Storage[user].Set(StorageKey.LastPlayingDate, DateTimeOffset.Now);
+		}
+
+		public static void OnHarassment(this Server core, IUser user, int decrementation = 1)
+		{
+			core.Dislike(user.Id, decrementation);
+			core.Storage[user].Set(StorageKey.HarrasmentedCount, core.Storage[user].Get(StorageKey.HarrasmentedCount, 0) + decrementation);
+		}
+
+		public static bool TryApologyze(this Server core, IUser user, int incrementation = 1)
+		{
+			if (core.Storage[user].Get(StorageKey.HarrasmentedCount, 0) < 3)
+			{
+				core.Like(user.Id, incrementation);
+				return true;
+			}
+			else
+				return false;
+		}
+	}
+
+}
