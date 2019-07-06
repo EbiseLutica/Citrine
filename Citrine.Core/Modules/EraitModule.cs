d2ca050b7837b93c0295abba8128ac1778bb4a35
Xeltica <xeltica@gmail.com>
2019-07-07 00:09:47 +0900

    えらい

---
+++ Citrine.Core/Modules/EraitModule.cs
@@ -0,0 +1,28 @@
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Citrine.Core.Api;
+
+namespace Citrine.Core.Modules
+{
+    public class EraitModule : ModuleBase
+	{
+		public override async Task<bool> ActivateAsync(IPost n, IShell shell, Server core)
+		{
+			if (n.Text == null)
+				return false;
+			var reg = Regex.Match(n.Text, @"(.+)(から褒めて|の(えら|偉)い)");
+			var reg2 = Regex.Match(n.Text, @"褒めて|(えら|偉)い\?？)");
+			if (reg.Success)
+			{
+                await shell.ReplyAsync(n, $"{reg.Groups[1].Value}のえらい!");
+				return true;
+			}
+			else if (reg2.Success)
+			{
+				await shell.ReplyAsync(n, "えらい!");
+				return true;
+			}
+			return false;
+		}
+	}
+}
