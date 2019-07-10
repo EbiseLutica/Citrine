b8f373be640905e2798aa08183a415355c10751f
Xeltica <xeltica@gmail.com>
2019-07-10 22:16:59 +0900

    UserStorage の改良

--- Citrine.Core/Storage/UserStorage.cs
+++ Citrine.Core/Storage/UserStorage.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.IO;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Citrine.Core.Api
 {
@@ -56,29 +54,29 @@ namespace Citrine.Core.Api
         {
             lock (storage)
             {
-                var obj = new Dictionary<string, Dictionary<string, object>>();
+                var obj = new JObject();
                 foreach (var kv in storage)
                 {
                     obj[kv.Key] = kv.Value.InternalRecord;
                 }
-                return JsonConvert.SerializeObject(obj);
+                return obj.ToString();
             }
         }
 
         private void DeserializeStorage(string json)
         {
-            var obj = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, object>>>(json);
+            var obj = JObject.Parse(json);
             lock (storage)
             {
                 foreach (var kv in obj)
                 {
-                    storage[kv.Key] = CreateRecord(kv.Value);
+                    storage[kv.Key] = CreateRecord(kv.Value as JObject);
                     logger.Debug($"Set {kv.Key}");
                 }
             }
         }
 
-        private UserRecord CreateRecord(Dictionary<string, object> r = null)
+        private UserRecord CreateRecord(JObject r = null)
         {
             if (Config.Instance.LoggingLevel <= LoggingLevel.Debug)
             {
@@ -106,9 +104,9 @@ namespace Citrine.Core.Api
         public class UserRecord
         {
             public UserRecord() { }
-            public UserRecord(Dictionary<string, object> r) => record = r;
+            public UserRecord(JObject obj) => record = obj;
 
-            internal Dictionary<string, object> InternalRecord => record;
+            internal JObject InternalRecord => record;
 
             public T Get<T>(string key, T defaultValue = default)
             {
@@ -117,56 +115,24 @@ namespace Citrine.Core.Api
                     return defaultValue;
                 
                 // 正しい型の値があれば返す　なければデフォルト
-                var retvalue = record[key];
                 try
                 {
-                    switch (defaultValue)
-                    {
-                        case int _:
-                            return (T)(object)Convert.ToInt32(retvalue);
-                        case long _:
-                            return (T)(object)Convert.ToInt64(retvalue);
-                        case short _:
-                            return (T)(object)Convert.ToInt16(retvalue);
-                        case byte _:
-                            return (T)(object)Convert.ToByte(retvalue);
-                        case uint _:
-                            return (T)(object)Convert.ToUInt32(retvalue);
-                        case ulong _:
-                            return (T)(object)Convert.ToUInt64(retvalue);
-                        case ushort _:
-                            return (T)(object)Convert.ToUInt16(retvalue);
-                        case sbyte _:
-                            return (T)(object)Convert.ToSByte(retvalue);
-                        case float _:
-                            return (T)(object)Convert.ToSingle(retvalue);
-                        case double _:
-                            return (T)(object)Convert.ToDouble(retvalue);
-                        case decimal _:
-                            return (T)(object)Convert.ToDecimal(retvalue);
-                        case string _:
-                            return (T)(object)(retvalue.ToString());
-                        default:
-                            return (T)retvalue;
-                    }
+                    return record[key].ToObject<T>();
                 }
-                catch (InvalidCastException)
+                catch (ArgumentException ex)
                 {
-                    logger.Warn($"Failed to get value {key} as {typeof(T).Name}");
+                    logger.Debug($"{ex.GetType().Name}: {ex.Message}\n{ex.StackTrace}");
                     return default;
                 }
             }
 
             public bool Has(string key) => record.ContainsKey(key);
 
-            public bool Is<T>(string key) => Has(key) && record[key] is T;
+            public bool Is<T>(string key) => Has(key);
 
             public void Set<T>(string key, T value)
             {
-                // 同値が登録してあればreturn
-                if (Has(key) && record[key] == (object)value)
-                    return;
-                record[key] = value;
+                record[key] = JToken.FromObject(value);
                 
                 Updated?.Invoke();
             }
@@ -180,13 +146,13 @@ namespace Citrine.Core.Api
 
             public void ClearAll()
             {
-                record.Clear();
+                record.RemoveAll();
                 Updated?.Invoke();
             }
 
             public event Action Updated;
 
-            private Dictionary<string, object> record = new Dictionary<string, object>();
+            private JObject record = new JObject();
         }
     }
 }
