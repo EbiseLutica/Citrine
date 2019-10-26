042ffeb3c1cf20f70d815987bd267c9f7ac28443
Xeltica <xeltica@gmail.com>
2019-10-27 04:47:47 +0900

    bump version

--- Citrine.Misskey/Shell.cs
+++ Citrine.Misskey/Shell.cs
@@ -23,7 +23,7 @@ namespace Citrine.Misskey
 
 	public class Shell : IShell
 	{
-		public static string Version => "2.2.0";
+		public static string Version => "2.2.1";
 
 		public MisskeyClient Misskey { get; private set; }
 
