9f85c36fd1233ec241bd2b9ff3098d9b9c3e45e9
Xeltica <xeltica@gmail.com>
2020-01-25 02:37:38 +0900

    嫌いな人を褒めないように

--- Citrine.Core/Modules/EraitModule.cs
+++ Citrine.Core/Modules/EraitModule.cs
@@ -4,17 +4,19 @@ using Citrine.Core.Api;
 
 namespace Citrine.Core.Modules
 {
-    public class EraitModule : ModuleBase
+	public class EraitModule : ModuleBase
 	{
 		public override async Task<bool> ActivateAsync(IPost n, IShell shell, Server core)
 		{
 			if (n.Text == null)
 				return false;
+			if (core.GetRatingOf(n.User) == Rating.Hate)
+				return true;
 			var reg = Regex.Match(n.Text.TrimMentions(), @"(.+)(から褒めて|の(えら|偉)い)");
 			var reg2 = Regex.Match(n.Text.TrimMentions(), @"褒めて|(えら|偉)い\?？");
 			if (reg.Success)
 			{
-                await shell.ReplyAsync(n, $"{reg.Groups[1].Value}のえらい!");
+				await shell.ReplyAsync(n, $"{reg.Groups[1].Value}のえらい!");
 				return true;
 			}
 			else if (reg2.Success)
