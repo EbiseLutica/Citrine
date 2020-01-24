daef22e32b5c9b742190ce266fe3384123bc9876
Xeltica <xeltica@gmail.com>
2020-01-25 02:37:45 +0900

    Pay API

--- Citrine.Core/Modules/EconomyModule.cs
+++ Citrine.Core/Modules/EconomyModule.cs
@@ -7,8 +7,6 @@ namespace Citrine.Core.Modules
 	public class EconomyModule : ModuleBase
 	{
 
-		public override int Priority => -10001;
-
 		public override async Task<bool> ActivateAsync(IPost n, IShell shell, Server core)
 		{
 			if (n.Text is string text)
@@ -17,19 +15,17 @@ namespace Citrine.Core.Modules
 				{
 					var balance = core.Storage[n.User].Get("economy.balance", 0);
 					await shell.ReplyAsync(n, $"{core.GetNicknameOf(n.User)}の所持金は, {balance} クォーツです!");
-					return true;
 				}
 				if (text.IsMatch("[買か]い(物|もの)"))
 				{
 					await shell.ReplyAsync(n, "クォーツショップはまだ開店準備中だよ. もう少しだけ待っててね");
-					return true;
 				}
+				return true;
 			}
-			Pay(n, shell, core);
 			return false;
 		}
 
-		private void Pay(IPost n, IShell shell, Server core)
+		public static void Pay(IPost n, IShell shell, Server core)
 		{
 			// 好感度に応じて、ランダムな量のお金をあげる
 			var storage = core.Storage[n.User];
@@ -46,6 +42,6 @@ namespace Citrine.Core.Modules
 			storage.Set("economy.balance", balance + addition);
 		}
 
-		private readonly Random rnd = new Random();
+		private static readonly Random rnd = new Random();
 	}
 }
